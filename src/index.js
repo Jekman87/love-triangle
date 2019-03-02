9337fdded3bb5cd3763c4954212a10c45db62ab3
Jekman <jeka_zzz@mail.ru>
2019-03-02 06:09:14 +0300

    feat: add function which calculates how many love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let SpichoneeA, SpichoneeB, SpichoneeC, i;
+  let triangles = 0;
+
+  for (i=0; i<preferences.length; i++) {
+    SpichoneeA = preferences[i];
+    SpichoneeB = preferences[SpichoneeA-1];
+    SpichoneeC = preferences[SpichoneeB-1];
+    if (i+1 == SpichoneeC && i+1 != SpichoneeA) triangles++;    
+  }
+  return triangles / 3;
 };
+
